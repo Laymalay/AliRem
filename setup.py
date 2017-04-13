73151f8ceea0fb5cebea8788eabfc558915957f5
Laymalay <laymalay@gmail.com>
2017-04-13 13:01:26 +0300

    Add Vagrantfile & Add requirements.txt

--- setup.py
+++ setup.py
@@ -3,9 +3,13 @@
 from os.path import join, dirname
 from setuptools import setup, find_packages
 
+with open('requirements.txt') as requirements_file:
+    install_requirements = requirements_file.read().splitlines()
+
 setup(
     name='core',
     version='1.0',
+    install_requires=install_requirements,
     packages=find_packages(),
     long_description=open(join(dirname(__file__), 'README.txt')).read(),
     test_suite='my_tests.test_remove',
---
+++ vagrant/README.md
@@ -0,0 +1,13 @@
+# Vagrant file for testing magic_rm tool  in Virtualbox VM #
+## Firts up VM ##
+```
+vagrant up
+```
+## SSH in to VM ##
+```
+vagrant ssh
+```  
+## Install/Reinstall tool in VM ##
+```
+vagrant provision
+```
